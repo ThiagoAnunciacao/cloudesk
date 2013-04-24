ad31fe74b97e6c4dc40b9ef240cd12f100e7c6c7
vquaiato <vinicius.quaiato@gmail.com>
2013-04-24 11:21:44 -0300

    new deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,11 +17,11 @@ set :normalize_asset_timestamps, false
 
 set :user,            "deployer"
 set :group,           "staff"
-set :use_sudo,        false
+set :use_sudo,        true
 
 role :web,    "67.207.152.23"
 role :app,    "67.207.152.23"
-role :db,     "67.207.152.23", :primary => true
+role :db,     "db.cloudesk.com.br", :primary => true
 
 set(:latest_release)  { fetch(:current_path) }
 set(:release_path)    { fetch(:current_path) }
@@ -41,100 +41,143 @@ default_environment["RUBY_VERSION"] = "ruby-1.9.3-p392"
 
 default_run_options[:shell] = 'bash'
 
+# namespace :deploy do
+#   desc "Deploy your application"
+#   task :default do
+#     update
+#     restart
+#   end
+
+#   desc "Setup your git-based deployment app"
+#   task :setup, :except => { :no_release => true } do
+#     dirs = [deploy_to, shared_path]
+#     dirs += shared_children.map { |d| File.join(shared_path, d) }
+#     run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
+#     run "git clone #{repository} #{current_path}"
+#   end
+
+#   task :cold do
+#     update
+#     migrate
+#   end
+
+#   task :update do
+#     transaction do
+#       update_code
+#     end
+#   end
+
+#   desc "Update the deployed code."
+#   task :update_code, :except => { :no_release => true } do
+#     run "cd #{current_path}; git fetch origin; git reset --hard #{branch}"
+#     finalize_update
+#   end
+
+#   desc "Update the database (overwritten to avoid symlink)"
+#   task :migrations do
+#     transaction do
+#       update_code
+#     end
+#     migrate
+#     restart
+#   end
+
+#   task :finalize_update, :except => { :no_release => true } do
+#     run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
+
+#     # mkdir -p is making sure that the directories are there for some SCM's that don't
+#     # save empty folders
+#     run <<-CMD
+#       rm -rf #{latest_release}/log #{latest_release}/public/system #{latest_release}/tmp/pids &&
+#       mkdir -p #{latest_release}/public &&
+#       mkdir -p #{latest_release}/tmp &&
+#       ln -s #{shared_path}/log #{latest_release}/log &&
+#       ln -s #{shared_path}/system #{latest_release}/public/system &&
+#       ln -s #{shared_path}/tmp/pids #{latest_release}/tmp/ &&
+#       ln -sf #{shared_path}/database.yml #{latest_release}/config/database.yml
+#     CMD
+
+#     if fetch(:normalize_asset_timestamps, true)
+#       stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
+#       asset_paths = fetch(:public_children, %w(images stylesheets javascripts)).map { |p| "#{latest_release}/public/#{p}" }.join(" ")
+#       run "find #{asset_paths} -exec touch -t #{stamp} {} ';'; true", :env => { "TZ" => "UTC" }
+#     end
+#   end
+
+#   desc "Zero-downtime restart of Unicorn"
+#   task :restart, :except => { :no_release => true } do
+#     run "kill -s USR2 `cat /tmp/pids/unicorn.teste.pid`"
+#   end
+
+#   desc "Start unicorn"
+#   task :start, :except => { :no_release => true } do
+#     run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
+#   end
+
+#   desc "Stop unicorn"
+#   task :stop, :except => { :no_release => true } do
+#     run "kill -s QUIT `cat /tmp/pids/unicorn.teste.pid`"
+#   end
+
+#   namespace :rollback do
+#     desc "Moves the repo back to the previous version of HEAD"
+#     task :repo, :except => { :no_release => true } do
+#       set :branch, "HEAD@{1}"
+#       deploy.default
+#     end
+
+#     desc "Rewrite reflog so HEAD@{1} will continue to point to at the next previous release."
+#     task :cleanup, :except => { :no_release => true } do
+#       run "cd #{current_path}; git reflog delete --rewrite HEAD@{1}; git reflog delete --rewrite HEAD@{1}"
+#     end
+
+#     desc "Rolls back to the previously deployed version."
+#     task :default do
+#       rollback.repo
+#       rollback.cleanup
+#     end
+#   end
+# end
+
 namespace :deploy do
-  desc "Deploy your application"
-  task :default do
-    update
-    restart
-  end
-
-  desc "Setup your git-based deployment app"
-  task :setup, :except => { :no_release => true } do
-    dirs = [deploy_to, shared_path]
-    dirs += shared_children.map { |d| File.join(shared_path, d) }
-    run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
-    run "git clone #{repository} #{current_path}"
-  end
-
-  task :cold do
-    update
-    migrate
-  end
-
-  task :update do
-    transaction do
-      update_code
-    end
-  end
-
-  desc "Update the deployed code."
-  task :update_code, :except => { :no_release => true } do
-    run "cd #{current_path}; git fetch origin; git reset --hard #{branch}"
-    finalize_update
-  end
-
-  desc "Update the database (overwritten to avoid symlink)"
-  task :migrations do
-    transaction do
-      update_code
-    end
-    migrate
-    restart
-  end
-
-  task :finalize_update, :except => { :no_release => true } do
-    run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
-
-    # mkdir -p is making sure that the directories are there for some SCM's that don't
-    # save empty folders
-    run <<-CMD
-      rm -rf #{latest_release}/log #{latest_release}/public/system #{latest_release}/tmp/pids &&
-      mkdir -p #{latest_release}/public &&
-      mkdir -p #{latest_release}/tmp &&
-      ln -s #{shared_path}/log #{latest_release}/log &&
-      ln -s #{shared_path}/system #{latest_release}/public/system &&
-      ln -s #{shared_path}/tmp/pids #{latest_release}/tmp/ &&
-      ln -sf #{shared_path}/database.yml #{latest_release}/config/database.yml
-    CMD
-
-    if fetch(:normalize_asset_timestamps, true)
-      stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
-      asset_paths = fetch(:public_children, %w(images stylesheets javascripts)).map { |p| "#{latest_release}/public/#{p}" }.join(" ")
-      run "find #{asset_paths} -exec touch -t #{stamp} {} ';'; true", :env => { "TZ" => "UTC" }
-    end
-  end
-
   desc "Zero-downtime restart of Unicorn"
-  task :restart, :except => { :no_release => true } do
-    run "kill -s USR2 `cat /home/deployer/apps/teste/shared/tmp/pids/unicorn.teste.pid`"
+  task :restart, roles: :app, :except => { :no_release => true } do
+    run "kill -s USR2 `cat /tmp/unicorn.teste.pid`"
   end
-
-  desc "Start unicorn"
-  task :start, :except => { :no_release => true } do
+ 
+  desc "Start Unicorn"
+  task :start, roles: :app, :except => { :no_release => true } do
     run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
   end
-
-  desc "Stop unicorn"
-  task :stop, :except => { :no_release => true } do
-    run "kill -s QUIT `cat /home/deployer/apps/teste/shared/tmp/pids/unicorn.teste.pid`"
-  end
-
-  namespace :rollback do
-    desc "Moves the repo back to the previous version of HEAD"
-    task :repo, :except => { :no_release => true } do
-      set :branch, "HEAD@{1}"
-      deploy.default
-    end
-
-    desc "Rewrite reflog so HEAD@{1} will continue to point to at the next previous release."
-    task :cleanup, :except => { :no_release => true } do
-      run "cd #{current_path}; git reflog delete --rewrite HEAD@{1}; git reflog delete --rewrite HEAD@{1}"
-    end
-
-    desc "Rolls back to the previously deployed version."
-    task :default do
-      rollback.repo
-      rollback.cleanup
+ 
+  desc "Stop Unicorn"
+  task :stop, roles: :app, :except => { :no_release => true } do
+    run "kill -s QUIT `cat /tmp/unicorn.teste.pid`"
+  end 
+ 
+  namespace :assets do
+    desc <<-DESC
+      Run the asset precompilation rake task. You can specify the full path \
+      to the rake executable by setting the rake variable. You can also \
+      specify additional environment variables to pass to rake via the \
+      asset_env variable. The defaults are:
+ 
+        set :rake,      "rake"
+        set :rails_env, "production"
+        set :asset_env, "RAILS_GROUPS=assets"
+ 
+      * only runs if assets have changed (add `-s force_assets=true` to force precompilation)
+    DESC
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      # Only precompile assets if any assets changed
+      # http://www.bencurtis.com/2011/12/skipping-asset-compilation-with-capistrano/
+      from = source.next_revision(current_revision)
+      if fetch(:force_assets, false) || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ lib/assets/ | wc -l").to_i > 0
+        # Just like original: https://github.com/capistrano/capistrano/blob/master/lib/capistrano/recipes/deploy/assets.rb
+        run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile"
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
     end
   end
 end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,11 +2,11 @@ env = ENV["RAILS_ENV"] || "development"
 
 # See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
 # documentation.
-worker_processes 4
+worker_processes 2
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen "/home/deployer/apps/teste/shared/tmp/pids/teste.socket", :backlog => 64
+listen "/tmp/teste.socket", :backlog => 64
 
 # Preload our app for more speed
 preload_app true
@@ -14,7 +14,7 @@ preload_app true
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
 
-pid "/home/deployer/apps/teste/shared/tmp/pids/unicorn.teste.pid"
+pid "/tmp/pids/unicorn.teste.pid"
 
 # Production specific settings
 if env == "production"
@@ -39,7 +39,7 @@ before_fork do |server, worker|
 
   # Before forking, kill the master process that belongs to the .oldbin PID.
   # This enables 0 downtime deploys.
-  old_pid = "/home/deployer/apps/teste/shared/tmp/pids/unicorn.teste.pid.oldbin"
+  old_pid = "/tmp/pids/unicorn.teste.pid.oldbin"
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
